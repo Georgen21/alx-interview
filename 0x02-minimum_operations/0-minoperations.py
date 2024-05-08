eedd8a5994b5aae302fef15c72022e2b538ebcd9
Georgen21 <chukwudinwabia42@gmail.com>
2024-05-09 00:50:52 +0100

    operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""
-This module defines a function that calculates the fewest number of 
-operations needed to result in exactly n H characters in the a given file.
-"""
+""" This module defines a function that calculates the fewest number of
+operations needed to result in exactly n H characters in the a given file. """
 
 
 def minOperations(n):
-    """
-    Returns the fewest number of operations needed to result in 
-    exactly n H characters in a given file.
-    """
+    """ Returns the fewest number of operations needed to result in
+    exactly n H characters in a given file. """
+
     if type(n) is not int or n <= 1:
         return 0
     factor = []
-
     divisor = 2
     while (n % divisor) == 0 and (n // divisor) != 1:
         factor.append(divisor)
@@ -25,5 +21,4 @@ def minOperations(n):
             n = n // divisor
         divisor += 2
     factor.append(n)
-
     return sum(factor)
